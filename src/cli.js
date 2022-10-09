79b736de73af1d8e2ad7ba42bbb594e5bba23318
Cezarine <96452545+Cezarine@users.noreply.github.com>
2022-10-09 12:25:36 -0300

    Criando um caminho pronto no package.json

--- src/cli.js
+++ src/cli.js
@@ -4,15 +4,27 @@ import pegaArquivo  from './index.js';
 
 const caminho = process.argv;
 
-function ImpremeList(pResultado)
+function ImpremeList(pResultado, pIdentificador='')
 {
-    console.log(chalk.yellow("Lista de Links: "), pResultado);   
+    console.log(
+        chalk.yellow("Lista de Links: "), 
+        chalk.black.bgGreen(pIdentificador),
+        pResultado);   
 }
 
 
 async function processaTexto(argumentos)
 {
     const caminho = argumentos[2];
+    try {
+        fs.lstatSync(caminho);
+    }   catch (error) {
+        if (error.code == "ENOENT") {
+            console.log(chalk.red("Diretorio ou caminho nao encontrado!!!"));
+            return ;
+        }  
+    }
+    
     if (fs.lstatSync(caminho).isFile())
     {
         const vResultado = await pegaArquivo(caminho);
@@ -23,12 +35,13 @@ async function processaTexto(argumentos)
         const arquivos = await fs.promises.readdir(caminho);
         arquivos.forEach(async (nomeArquivo) => {
             const list = await pegaArquivo(`${caminho}/${nomeArquivo}`);
-           // ImpremeList(`${caminho}/${nomeArquivo}`);
-            ImpremeList(list);
-        });// Para cada elemento(caminho) dentro de arquivos(vou chama-los de "nomeArquivo") vou chamar a function "pegaArquivo" passando
+            // ImpremeList(`${caminho}/${nomeArquivo}`);
+            ImpremeList(list, nomeArquivo);
+        });
+        // Para cada elemento(caminho) dentro de arquivos(vou chama-los de "nomeArquivo") vou chamar a function "pegaArquivo" passando
         // como parametro (caminho/nomeArquivo)="/arquivos/texto.txt"
-        //console.log(arquivos);
-    }
+        //console.log(arquivos).  
+    } 
 }
 
 processaTexto(caminho);
